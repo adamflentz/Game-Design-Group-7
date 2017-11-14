3c7221eb72ed95efefa2763790aaae9687d1f84d
adamflentz <aflentz@email.wm.edu>
2017-11-14 02:19:38 -0500

    blank screen boy

--- include/game/screens/GameplayScreen.hpp
+++ include/game/screens/GameplayScreen.hpp
@@ -31,7 +31,7 @@ public:
     void onDraw(sf::RenderTarget& ctx, sf::RenderStates states) const;
 protected:
     void createViews(int numPlayers);
-    int numplayers = 3;
+    int numplayers = 1;
     std::vector<std::shared_ptr<Character>> activeCharacters;
     // std::vector<std::unique_ptr<Room> > roomFac;
     RoomGroup group;
