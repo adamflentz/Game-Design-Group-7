085e8d095a6a821d1102a30ed44cba5dc5e0032c
Wentao  Xu <wxu02@anchor.cs.wm.edu>
2017-12-04 15:59:01 -0500

    made about 15 tile maps for rooms, need more

---
+++ resources/room/Interior1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Interior1" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../../Documents/Game-Design-Group-7/resources/sprites/Interior1.png" width="1024" height="1024"/>
+</tileset>
---
+++ resources/room/WaterAndFire.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="WaterAndFire" tilewidth="32" tileheight="32" tilecount="48" columns="6">
+ <image source="../../Documents/Game-Design-Group-7/resources/sprites/WaterAndFire.png" width="192" height="256"/>
+</tileset>
